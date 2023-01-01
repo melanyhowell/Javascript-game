b444b89990f99062a708fe952fa2314625bb29f0
melanyhowell <melany.r.howell@gmail.com>
2023-01-01 14:24:08 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const animals = [
+    { name: "squirel", image: "/assets/images/squirrel.png" },
+    { name: "sheep", image: "/assets/images/sheep.png" },
+    { name: "rooster", image: "/assets/images/rooster.png" },
+    { name: "pig", image: "/assets/images/pig.png" },
+    { name: "kangaroo", image: "/assets/images/kangaroo.png" },
+    { name: "hippo", image: "/assets/images/hippo.png" },
+    { name: "goat", image: "/assets/images/goat.png" },
+    { name: "elephant", image: "/assets/images/elephant.png" },
+    { name: "duck", image: "/assets/images/duck.png" },
+    { name: "donkey", image: "/assets/images/donkey.png" },
+  ];
